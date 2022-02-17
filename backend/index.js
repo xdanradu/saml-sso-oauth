03550455012113eb172ef98984fca66133184072
radud1 <dan.radu@msg.group>
2022-02-17 09:02:24 +0200

    v2 Cleanup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let cors = require('cors');
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 let app = express();
 app.use(cors());
 app.use(cookieParser());
@@ -8,42 +8,45 @@ const path = require('path');
 
 let bodyParser = require('body-parser');
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-    console.log(`Backend API ${request.headers.host} `);
-    response.json('API');
+  console.log(`Backend API ${request.headers.host} `);
+  response.json('API');
 });
 
 app.get('/saml/oauth', (request, response) => {
-    console.log('saml/oauth');
-    // if we don't have an authorization header then we redirect to IDP
-    if(request.headers.authorization) {
-        console.dir(request.headers.authorization);
-        response.send(200);
-    } else {
-        response.redirect('http://localhost:3000/external-idp');
-    }
-
+  console.log('saml/oauth');
+  // if we don't have an authorization header then we redirect to IDP
+  if (request.headers.authorization) {
+    console.dir(request.headers.authorization);
+    response.send(200);
+  } else {
+    response.redirect('http://localhost:3000/external-idp');
+  }
 });
 
 app.post('/saml/sso', (request, response) => {
-    console.log('POST: We already have the cookie token in this POST endpoint but we redirect to GET');
-    console.log(request.cookies.token);
-    response.redirect('http://localhost:3007/auth');
+  console.log(
+    'POST: We already have the cookie token in this POST endpoint but we redirect to GET'
+  );
+  console.log(request.cookies.token);
+  response.redirect('http://localhost:3007/auth');
 });
 
 app.get('/auth', (request, response) => {
-    console.log('GET: Now we can create a new session on the backend for the current user and return the REST api access token to the FE');
-    console.log(request.cookies.token);
-    // We dinamically inject the token into the login.html file (in the span) - hardcoded for the PoC
-    response.sendfile(path.join(__dirname+'/views/login.html'));
+  console.log(
+    'GET: Now we can create a new session on the backend for the current user and return the REST api access token to the FE'
+  );
+  console.log(request.cookies.token);
+  // We dinamically inject the token into the login.html file (in the span) - hardcoded for the PoC
+  response.sendfile(path.join(__dirname + '/views/login.html'));
 });
 
-app.listen(3007, function () {
-    console.log('Server running @ localhost:3007');
+app.listen(3007, function() {
+  console.log('Server running @ localhost:3007');
 });
--- frontend/app.js
+++ frontend/app.js
@@ -1,178 +1,111 @@
 let popupWindow;
-//let url = 'http://localhost:3000/external-idp';
 let url = 'http://localhost:3007/saml/oauth';
-let showScrollbars = true;
-let onAbortCallback;
-let onSuccessCallback;
 let closeDetectionInterval;
-let messagingInterval;
+let messagesToChildInterval;
+let accessToken;
 
 function login() {
+  if (accessToken) {
+    console.log(`Already logged in with token: ${accessToken}`);
+    console.log('Sending auth test request to backend');
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
 
-    if(accessToken) {
-        console.log('Already logged in');
-        window.addEventListener('message', event => {
-            handleMessageEvent(event);
-        });
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.setRequestHeader("Authorization", "Bearer "+accessToken);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-            }};
-        xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        console.log(`Status: ${xhr.status} Response texT: ${xhr.responseText}`);
+      }
+    };
+    xhr.send();
+  } else {
+    if (popupWindow != null && popupWindow.focus) {
+      popupWindow.focus();
     } else {
-        if (popupWindow != null && popupWindow.focus) {
-            popupWindow.focus();
-        } else {
-            const features = generateWindowFeaturesString();
-            // console.log(features);
-            popupWindow = window.open(
-                url,
-                'myWindow',
-                features
-            );
-            if (popupWindow.focus) {
-                popupWindow.focus();
-            }
-            registerClosingDetection();
-            initiateMessagingWithChild();
-        }
+      const features = generateWindowFeaturesString();
+      popupWindow = window.open(url, 'myWindow', features);
+      if (popupWindow.focus) {
+        popupWindow.focus();
+      }
+      closePopupListener();
+      initiateMessagingWithChild();
     }
-
-
+  }
 }
 
 function generateWindowFeaturesString() {
-    const dualScreenLeft =
-        window.screenLeft !== undefined
-            ? window.screenLeft
-            : window.screenX;
-    const dualScreenTop =
-        window.screenTop !== undefined
-            ? window.screenTop
-            : window.screenY;
-    const width = window.innerWidth
-        ? window.innerWidth
-        : document.documentElement.clientWidth
-            ? document.documentElement.clientWidth
-            : screen.width;
-    const height = window.innerHeight
-        ? window.innerHeight
-        : document.documentElement.clientHeight
-            ? document.documentElement.clientHeight
-            : screen.height;
+  const dualScreenLeft =
+    window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+  const dualScreenTop =
+    window.screenTop !== undefined ? window.screenTop : window.screenY;
+  const width = window.innerWidth
+    ? window.innerWidth
+    : document.documentElement.clientWidth
+    ? document.documentElement.clientWidth
+    : screen.width;
+  const height = window.innerHeight
+    ? window.innerHeight
+    : document.documentElement.clientHeight
+    ? document.documentElement.clientHeight
+    : screen.height;
 
-    // Calculate popup settings
-    const left = width / 2 - width / 2 + dualScreenLeft;
-    const top = height / 2 - height / 2 + dualScreenTop;
-    const scrollbars = showScrollbars ? 'yes' : 'no';
+  // Calculate popup settings
+  const left = width / 2 - width / 2 + dualScreenLeft;
+  const top = height / 2 - height / 2 + dualScreenTop;
+  const scrollbars = 'yes';
 
-    // Generate popup features string
-    return `scrollbars=${scrollbars}, width=${width/2}, height=${height}, top=${top}, left=${left}`;
+  // Generate popup features string
+  return `scrollbars=${scrollbars}, width=${width /
+    2}, height=${height}, top=${top}, left=${left}`;
 }
 
 function initiateMessagingWithChild() {
-    messagingInterval = window.setInterval(() => {
-        try {
-            //console.log(popupWindow);
-            if (popupWindow != null && !popupWindow.closed) {
-                // console.log(popupWindow);
-                console.log('Ask if child is active');
-                popupWindow.postMessage('Are you alive?', '*');
-            }
-
-        } catch (e) {}
-    }, 2000);
+  messagesToChildInterval = window.setInterval(() => {
+    try {
+      //console.log(popupWindow);
+      if (popupWindow != null && !popupWindow.closed) {
+        // console.log(popupWindow);
+        console.log('Ask if child is active');
+        popupWindow.postMessage('Are you alive?', '*');
+      }
+    } catch (e) {}
+  }, 2000);
 }
 
-function registerClosingDetection() {
-    console.log('should add listener on close');
-    closeDetectionInterval = window.setInterval(() => {
-        try {
-            // console.log(popupWindow);
-            if (popupWindow == null || popupWindow.closed) {
-                close();
-                console.log('close the popup');
-            }
-        } catch (e) {}
-    }, 2000);
+function closePopupListener() {
+  console.log('should add listener on close');
+  closeDetectionInterval = window.setInterval(() => {
+    try {
+      // console.log(popupWindow);
+      if (popupWindow == null || popupWindow.closed) {
+        close();
+        console.log('close the popup');
+      }
+    } catch (e) {}
+  }, 2000);
 }
 
 function close() {
-    console.log('close all');
-    window.clearInterval(closeDetectionInterval);
-    window.clearInterval(messagingInterval);
-    if (popupWindow != null) {
-        popupWindow.close();
-        popupWindow = null;
-    }
+  console.log('Close the popup and the periodic intervals');
+  window.clearInterval(closeDetectionInterval);
+  window.clearInterval(messagesToChildInterval);
+  if (popupWindow != null) {
+    popupWindow.close();
+    popupWindow = null;
+  }
 }
 
-let accessToken;
 function handleMessageEvent(event) {
-    // if(event.data.token) console.log(event.data.token);
-    if (event.data.token) {
-        console.log(`Token received => Spartacus auth`);
-        console.dir(event.data.token);
-        accessToken = event.data.token;
-        close();
-    }
+  if (event.data.token) {
+    console.log(`Token received => Auth`);
+    console.dir(event.data.token);
+    accessToken = event.data.token;
+    close();
+  }
 }
 
 window.addEventListener('message', event => {
-    handleMessageEvent(event);
+  handleMessageEvent(event);
 });
-
-
-
-
-
-
-/*
-function authorized() {
-    if(accessToken) {
-        window.addEventListener('message', event => {
-            handleMessageEvent(event);
-        });
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", 'http://localhost:3007/saml/oauth');
-
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.setRequestHeader("Authorization", "Bearer "+accessToken);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-            }};
-        xhr.send();
-    } else {
-        window.addEventListener('message', event => {
-            handleMessageEvent(event);
-        });
-
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", 'http://localhost:3007/saml/oauth');
-
-        xhr.setRequestHeader("Accept", "application/json");
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-            }};
-        xhr.send();
-    }
-
-
-
-}
-*/
